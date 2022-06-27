480a65e700027da9103e92ac014bfc765e43e49e
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-06-27 16:22:01 -0500

    Create HashtagDto.java
    Fixes #75

---
+++ src/main/java/com/cooksys/team3/dtos/HashtagDto.java
@@ -0,0 +1,18 @@
+package com.cooksys.team3.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+public class HashtagDto {
+
+    private String label;
+
+    private Timestamp firstUsed;
+
+    private Timestamp lastUsed;
+
+}
