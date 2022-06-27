40f59fd4bda38205c398ffad5a93078dddfc3c8a
Nissim Ram <nissimram1812@gmail.com>
2022-06-27 19:54:10 -0400

    Created HashtagController. fix #63

---
+++ src/main/java/com/cooksys/team3/controllers/HashtagController.java
@@ -0,0 +1,17 @@
+package com.cooksys.team3.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hashtag")
+
+
+
+public class HashtagController {
+
+	
+}
