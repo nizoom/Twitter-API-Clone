f2b444f3fd6c8827109e6674321bb2b5a7fd5898
iv-island <isaiah.boblett@gmail.com>
2022-06-27 13:46:03 -0700

    Created the Profile Embeddable. Fixes #79

---
+++ src/main/java/com/cooksys/team3/embeddables/Profile.java
@@ -0,0 +1,15 @@
+package com.cooksys.team3.embeddables;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Profile {
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String phone;
+}
