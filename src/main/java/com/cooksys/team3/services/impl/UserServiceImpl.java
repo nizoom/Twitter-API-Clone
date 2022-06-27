0b9689efc7f3bb215be173f3b653816f70844f7d
iv-island <isaiah.boblett@gmail.com>
2022-06-27 14:10:10 -0700

    Created UserService & UserServiceImpl. Fixes #56

---
+++ src/main/java/com/cooksys/team3/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cooksys.team3.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.cooksys.team3.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+}
