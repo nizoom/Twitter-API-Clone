81e9d58da23ff5e6408f6ea82c8ba815fb9df5de
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-06-27 16:10:47 -0500

    Update TweetRepository.java

--- src/main/java/com/cooksys/team3/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/team3/repositories/TweetRepository.java
@@ -1,2 +1,7 @@
-package com.cooksys.team3.repositories;public class TweetRepository {
+package com.cooksys.team3.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TweetRepository extends JpaRepository <Tweet, Long> {
 }
