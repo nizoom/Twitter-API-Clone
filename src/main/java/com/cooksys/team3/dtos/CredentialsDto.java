1e0bedc13ff7eb88a3268c54e23e82035ec9699f
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-06-27 16:27:54 -0500

    Create CredentialsDto.java
    Fixes #72

---
+++ src/main/java/com/cooksys/team3/dtos/CredentialsDto.java
@@ -0,0 +1,12 @@
+package com.cooksys.team3.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class CredentialsDto {
+
+    private String username;
+    private String password;
+}
