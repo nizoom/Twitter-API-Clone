6a501bfb1d6fe84a8e0d9f33ade39762bd55e186
Nissim Ram <nissimram1812@gmail.com>
2022-06-27 18:55:36 -0400

    Created Tweet Controller. Fix #64

---
+++ src/main/java/com/cooksys/team3/controllers/TweetController.java
@@ -0,0 +1,16 @@
+package com.cooksys.team3.controllers;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tweet")
+
+public class TweetController {
+
+}
