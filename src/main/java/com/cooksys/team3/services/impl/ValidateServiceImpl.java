3ac8245a8dc46c17984c2a424859318ce16b048b
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-06-27 16:32:35 -0500

    fixes #53
    created ValidateService and ValidateServiceImpl

---
+++ src/main/java/com/cooksys/team3/services/impl/ValidateServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cooksys.team3.services.impl;
+
+import com.cooksys.team3.services.ValidateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ValidateServiceImpl implements ValidateService {
+}
