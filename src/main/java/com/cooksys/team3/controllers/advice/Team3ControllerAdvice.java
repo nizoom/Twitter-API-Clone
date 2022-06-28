0dd6c494f0976655d932a0123bc05a3fc638e4bf
Christina Wilson <WilsonCN14@gmail.com>
2022-06-28 09:25:14 -0500

    Create @ControllerAdvice. Fixes #66

---
+++ src/main/java/com/cooksys/team3/controllers/advice/Team3ControllerAdvice.java
@@ -0,0 +1,8 @@
+package com.cooksys.team3.controllers.advice;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice(basePackages = {"com.cooksys.team3.controllers"})
+public class Team3ControllerAdvice {
+
+}
