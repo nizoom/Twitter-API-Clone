5422132553cc77e1d79ead375ddadfd15aefd428
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-06-28 10:45:01 -0500

    Update TweetRepository.java
    update imports

--- src/main/java/com/cooksys/team3/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/team3/repositories/TweetRepository.java
@@ -1,6 +1,7 @@
 package com.cooksys.team3.repositories;
 
 
+import com.cooksys.team3.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
