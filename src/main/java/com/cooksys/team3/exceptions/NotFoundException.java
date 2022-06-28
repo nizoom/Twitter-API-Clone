7e4383e3fbfd6fe79dd379c3b6129381273ba629
Christina Wilson <WilsonCN14@gmail.com>
2022-06-28 09:11:53 -0500

    Create NotFoundException. Fixes #47

---
+++ src/main/java/com/cooksys/team3/exceptions/NotFoundException.java
@@ -0,0 +1,16 @@
+package com.cooksys.team3.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+
+}
