fd2ea18892cef143f5e5dcab9d8648ff4c4fd0ff
Christina Wilson <WilsonCN14@gmail.com>
2022-06-29 19:14:05 -0500

    Updated RequestMapping in the controllers to match Assessment 1 Test Suite in Postman
    Postman uses /users and /tags.
    We were using /user and /hashtag so we were failing the Postman tests.

--- src/main/java/com/cooksys/team3/controllers/HashtagController.java
+++ src/main/java/com/cooksys/team3/controllers/HashtagController.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hashtag")
+@RequestMapping("/tags")
 
 
 
--- src/main/java/com/cooksys/team3/controllers/UserController.java
+++ src/main/java/com/cooksys/team3/controllers/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 	private final UserService userService;
 
