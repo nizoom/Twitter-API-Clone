1da1d4efd26b972b7b1dcb00dd157ccb991257d1
Christina Wilson <WilsonCN14@gmail.com>
2022-06-30 10:54:07 -0500

    Implemented GET tweets/id/likes. Fixes #5

--- src/main/java/com/cooksys/team3/controllers/TweetController.java
+++ src/main/java/com/cooksys/team3/controllers/TweetController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.team3.dtos.CredentialsDto;
 import com.cooksys.team3.dtos.TweetResponseDto;
 import com.cooksys.team3.dtos.UserRequestDto;
+import com.cooksys.team3.dtos.UserResponseDto;
 import com.cooksys.team3.services.TweetService;
 
 import lombok.RequiredArgsConstructor;
@@ -31,6 +31,11 @@ public class TweetController {
 	@GetMapping("/{id}")
 	public TweetResponseDto getTweetById(@PathVariable Long id) {
 		return tweetService.getTweetById(id);
+	}
+	
+	@GetMapping("/{id}/likes")
+	public List<UserResponseDto> getUsersWhoLikedTweet(@PathVariable Long id) {
+		return tweetService.getUsersWhoLikedTweet(id);
 	}
 	
 
