1c820c0b56807cad739ff7fd38a14bb108aafd75
Christina Wilson <WilsonCN14@gmail.com>
2022-07-01 12:47:24 -0500

    Added handeNotFoundException to controller advice

--- src/main/java/com/cooksys/team3/controllers/advice/Team3ControllerAdvice.java
+++ src/main/java/com/cooksys/team3/controllers/advice/Team3ControllerAdvice.java
@@ -1,8 +1,37 @@
 package com.cooksys.team3.controllers.advice;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.cooksys.team3.dtos.ErrorDto;
+import com.cooksys.team3.exceptions.BadRequestException;
+import com.cooksys.team3.exceptions.NotAuthorizedException;
+import com.cooksys.team3.exceptions.NotFoundException;
 
 @ControllerAdvice(basePackages = {"com.cooksys.team3.controllers"})
+@ResponseBody
 public class Team3ControllerAdvice {
 
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
+		return new ErrorDto(notFoundException.getMessage());
+	}
+	
+//	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+//	@ExceptionHandler(NotFoundException.class)
+//	public ErrorDto handleNotAuthorizedException(HttpServletRequest request, NotAuthorizedException notAuthorizedException) {
+//		return new ErrorDto(notAuthorizedException.getMessage());
+//	}
+//	
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	@ExceptionHandler(BadRequestException.class)
+//	public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+//		return new ErrorDto(badRequestException.getMessage());
+//	}
 }
--- src/main/java/com/cooksys/team3/dtos/ErrorDto.java
+++ src/main/java/com/cooksys/team3/dtos/ErrorDto.java
@@ -1,12 +1,14 @@
 package com.cooksys.team3.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ErrorDto {
-	
+
 	private String message;
 	
 }
