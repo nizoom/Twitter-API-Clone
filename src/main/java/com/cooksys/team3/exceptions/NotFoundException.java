22b3a2e8d2c54bff3cd36b74c56f15174dc24736
Darya Krutyeva <85214246+DaryaKrutyeva@users.noreply.github.com>
2022-07-01 12:27:28 -0500

    Implemented ResponseStatus, passing 140
    Implemented ResponseStatus

--- src/main/java/com/cooksys/team3/exceptions/NotFoundException.java
+++ src/main/java/com/cooksys/team3/exceptions/NotFoundException.java
@@ -3,7 +3,12 @@ package com.cooksys.team3.exceptions;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(
+		HttpStatus.NOT_FOUND
+)
 @AllArgsConstructor
 @Getter
 @Setter
